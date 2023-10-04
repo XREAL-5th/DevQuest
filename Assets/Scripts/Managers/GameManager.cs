815746205801d486364f566373a5fd3191ed210e
seunguri <safg0421@gmail.com>
2023-10-05 01:10:10 +0900

    feat: BaseScene, SceneMangerEx
    - GameScene, EndScene

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,31 +1,22 @@
 ﻿using UnityEngine;
 
-public class GameManger : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    static GameManger main;
 
     [HideInInspector] public PlayerControl player;
     [HideInInspector] public Camera cam;
     [HideInInspector] public CameraControl camc;
 
+    public static GameManager _main;
+
     public Transform playerSpawn;
-    public GameObject[] dialogs = { };
 
     //non-lazy, non-DDOL
     private void Awake()
     {
-        main = this;
+        _main = this;
         player = GameObject.FindObjectOfType<PlayerControl>();
         cam = Camera.main;
         camc = GameObject.FindObjectOfType<CameraControl>();
-    }
-
-    public bool DialogOpen()
-    {
-        foreach (GameObject dialog in dialogs)
-        {
-            if (dialog.activeInHierarchy) return true;
-        }
-        return false;
     }
 }
