024e9a8ac20809a42372d279850d9491819b9a31
seunguri <safg0421@gmail.com>
2023-10-04 21:04:41 +0900

    model: add buble blaster gun

---
+++ Assets/Scripts/Managers/GameManger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    static GameManger main;
+
+    [HideInInspector] public PlayerControl player;
+    [HideInInspector] public Camera cam;
+    [HideInInspector] public CameraControl camc;
+
+    public Transform playerSpawn;
+    public GameObject[] dialogs = { };
+
+    //non-lazy, non-DDOL
+    private void Awake()
+    {
+        main = this;
+        player = GameObject.FindObjectOfType<PlayerControl>();
+        cam = Camera.main;
+        camc = GameObject.FindObjectOfType<CameraControl>();
+    }
+
+    public bool DialogOpen()
+    {
+        foreach (GameObject dialog in dialogs)
+        {
+            if (dialog.activeInHierarchy) return true;
+        }
+        return false;
+    }
+}
