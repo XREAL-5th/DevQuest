adaf11d1dd0c5b8d91f2bfa165bf3169726d694d
wave-sound <lunar105630@gmail.com>
2023-10-04 19:39:42 +0900

    [Refactor] Removed Legacy Class

--- Assets/Scripts/ScaleItem.cs
+++ Assets/Scripts/ScaleItem.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GrowItem : Item
+public class ScaleItem : Item
 {
-    [SerializeField] private ItemData growItemData;
+    [SerializeField] private ItemData scaleItemData;
 
     private void Awake()
     {
-        itemData = growItemData;
+        itemData = scaleItemData;
     }
     public override void EffectPlayer(Player player)
     {
+        Debug.Log($"{itemData.Name}의 영향으로 크기가 변합니다!");
         Vector3 playerTransform = player.transform.localScale;
         player.transform.localScale = new Vector3(playerTransform.x, playerTransform.y, playerTransform.z) * itemData.Amount;
         gameObject.SetActive(false);
