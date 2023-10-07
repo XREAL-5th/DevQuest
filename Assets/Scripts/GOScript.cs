40525b6b28d9c3fd38bcdad0750b2042e7a2ea4c
U_Zin <930yujin@gmail.com>
2023-10-07 23:37:11 +0900

    [ADD] GameOver Scene

---
+++ Assets/Scripts/GOScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GOScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None; // 마우스 커서가 보이도록 설정
+        Cursor.visible = true; // 마우스 커서를 보이도록 설정 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
