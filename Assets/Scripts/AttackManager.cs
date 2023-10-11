814151efcd15cf92937d66161dba5cc5d64307ef
heejin5484 <heejin990308@gmail.com>
2023-10-12 04:14:30 +0900

    노희진_10.9_DevQuest’
    .

--- Assets/Scripts/AttackManager.cs
+++ Assets/Scripts/AttackManager.cs
@@ -7,9 +7,9 @@ using UnityEngine.UI;
 
 public class AttackManager : MonoBehaviour
 {
-    //public GameObject bulletFactory;
-    //public Transform firePosition;
     public ParticleSystem hiteffect;
+    public Image img_Skill;
+    public ParticleSystem skilleffect;
     
     void Start(){
 
@@ -21,16 +21,13 @@ public class AttackManager : MonoBehaviour
         {
             Shot();
         }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartCoroutine(CoolTime(5f));
+            SkillQ();
+        }
     }
 
-    /*
-    void ShotBullet()
-    {
-        GameObject bullet = Instantiate(bulletFactory);
-        bullet.transform.position = firePosition.position;
-        bullet.transform.forward = firePosition.transform.right;
-    }
-    */
 
     void Shot()
     {
@@ -52,4 +49,38 @@ public class AttackManager : MonoBehaviour
         Destroy(newParticleSystem.gameObject, newParticleSystem.main.duration);
         
     }
+
+    IEnumerator CoolTime(float cool)
+    {
+        Debug.Log("쿨타임 시작");
+
+       
+        float elapsedTime = 0;
+
+        while (elapsedTime < cool)
+        {
+            elapsedTime += Time.deltaTime;
+            img_Skill.fillAmount = elapsedTime / cool;
+            yield return null;
+        }
+
+        img_Skill.fillAmount = 1.0f; // 확실하게 1로 설정
+
+        Debug.Log("쿨타임 종료");
+
+
+    }
+
+    void SkillQ()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Physics.Raycast(ray, out RaycastHit hitInfo);
+
+        ParticleSystem newParticleSystem = Instantiate(skilleffect, hitInfo.point, Quaternion.identity);
+        newParticleSystem.Play();
+
+        Destroy(newParticleSystem.gameObject, newParticleSystem.main.duration);
+
+    }
+
 }
