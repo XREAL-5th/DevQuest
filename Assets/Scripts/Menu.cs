8ee207f68009014bfd2fc608f203947b153f4832
U_Zin <930yujin@gmail.com>
2023-10-27 18:00:30 +0900

    [ADD] Game Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None; // 마우스 커서가 보이도록 설정
+        Cursor.visible = true; // 마우스 커서를 보이도록 설정 
+    }
+    public void OnClickEndButton()
+    {
+        Debug.Log("Menu - Exit 누름");
+        SceneManager.LoadScene("GameOver");
+
+    }
+}
